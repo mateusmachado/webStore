056b9e577f4e3f2ee2e5e80fdcafbaa0e08e8dc7
Mateus <mmachado.csp@gmail.com>
2015-07-27 02:49:50 -0300

    deleted default return

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,19 +1,12 @@
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
 
 @Controller
 @EnableAutoConfiguration
 public class Application {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
-    }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
 }
