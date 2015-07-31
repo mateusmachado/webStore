043469d13a40857dc094154c7d3a71157f43850c
Mateus <mmachado.csp@gmail.com>
2015-07-30 23:55:33 -0300

    foo

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Controller;
 public class Application {
 
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args); 
 	}
 }
